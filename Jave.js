f056683453883e871977db972350e05e137066b8
Adriancito32 <adriancito.uqz@gmail.com>
2023-02-02 00:53:46 -0300

    adrem.pms

---
+++ Jave.js
@@ -0,0 +1,11 @@
+function cambiarFondo(){
+    let body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor= "#000000";
+
+}
+
+function cambiarFondo2(){
+    let body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor= "rgb(116, 0, 106)";
+
+}
